8bf882b78f38c74199844cca549cc9d8b98ae02f
pabloav57 <pabloav57@gmail.com>
2025-02-01 18:40:27 +0100

    Chasing the Player Part 2

--- Assets/State.cs
+++ Assets/State.cs
@@ -56,14 +56,13 @@ public class State
         }
         return this; // If we're not returning the nextState, then return the same state.
     }
-}
 
     public bool CanSeePlayer()
     {
-        Vector3 direction = player.position - npc.tranfom.position;
-        float angle = Vector3.Angle(direction, npc.tranform.forward);
+        Vector3 direction = player.position - npc.transform.position;
+        float angle = Vector3.Angle(direction, npc.transform.forward);
 
-        if(direction.magnitude < VisDist && angle < visAngle)
+        if (direction.magnitude < visDist && angle < visAngle)
         {
             return true;
         }
@@ -72,152 +71,192 @@ public class State
 
     public bool CanAttackPlayer()
     {
-        Vector3 direction = player.position - npc.tranfom.position;
-        if(direction.magnitude < shootDist)
+        Vector3 direction = player.position - npc.transform.position;
+        if (direction.magnitude < shootDist)
         {
             return true;
         }
         return false;
     }
 
-// Constructor for Idle state.
-public class Idle : State
-{
-    public Idle(GameObject _npc, NavMeshAgent _agent, Animator _anim, Transform _player)
-                : base(_npc, _agent, _anim, _player)
+    // Constructor for Idle state.
+    public class Idle : State
     {
-        name = STATE.IDLE; // Set name of current state.
-    }
-
-    public override void Enter()
-    {
-        anim.SetTrigger("isIdle"); // Sets any current animation state back to Idle.
-        base.Enter(); // Sets stage to UPDATE.
-    }
-    public override void Update()
-    {
-        // The only place where Update can break out of itself. Set chance of breaking out at 10%.
-        if (Random.Range(0,100) < 10)
+        public Idle(GameObject _npc, NavMeshAgent _agent, Animator _anim, Transform _player)
+                    : base(_npc, _agent, _anim, _player)
         {
-            nextState = new Patrol(npc, agent, anim, player);
-            stage = EVENT.EXIT; // The next time 'Process' runs, the EXIT stage will run instead, which will then return the nextState.
+            name = STATE.IDLE; // Set name of current state.
         }
-    }
 
-    public override void Exit()
-    {
-        anim.ResetTrigger("isIdle"); // Makes sure that any events queued up for Idle are cleared out.
-        base.Exit();
-    }
-}
-
-// Constructor for Patrol state.
-public class Patrol : State
-{
-    int currentIndex = -1;
-    public Patrol(GameObject _npc, NavMeshAgent _agent, Animator _anim, Transform _player)
-                : base(_npc, _agent, _anim, _player)
-    {
-        name = STATE.PATROL; // Set name of current state.
-        agent.speed = 2; // How fast your character moves ONLY if it has a path. Not used in Idle state since agent is stationary.
-        agent.isStopped = false; // Start and stop agent on current path using this bool.
-    }
-
-    public override void Enter()
-    {
-        currentIndex = 0; // This will start agent at the first waypoint.
-        anim.SetTrigger("isWalking"); // Start agent walking animation.
-        base.Enter();
-    }
-
-    public override void Update()
-    {
-        // Check if agent hasn't finished walking between waypoints.
-        if(agent.remainingDistance < 1)
+        public override void Enter()
         {
-            // If agent has reached end of waypoint list, go back to the first one, otherwise move to the next one.
-            if (currentIndex >= GameEnvironment.Singleton.Checkpoints.Count - 1)
-                currentIndex = 0;
-            else
-                currentIndex++;
-
-            agent.SetDestination(GameEnvironment.Singleton.Checkpoints[currentIndex].transform.position); // Set agents destination to position of next waypoint.
+            anim.SetTrigger("isIdle"); // Sets any current animation state back to Idle.
+            base.Enter(); // Sets stage to UPDATE.
         }
-    }
 
-    public override void Exit()
-    {
-        anim.ResetTrigger("isWalking"); // Makes sure that any events queued up for Walking are cleared out.
-        base.Exit();
-    }
-}
-
-public class Pursue: State{
-    public Pursue(GameObject _npc, NavMeshAgent _agent, Animator _anim, Transform _player)
-                : base(_npc, _agent, _anim, _player)
-                {
-                    name = State.STATE.PURSUE;
-                    agent.speed = 5;
-                    agent.isStopped = false;
-                }
-
-    public overrride void Enter()
-    {
-        anim.SetTrigger("isRunning");
-        base.Enter;
-    }
-    public overrride void Update()
-    {
-        agent.SetDestination(player.position);
-        if(agent.hasPath)
+        public override void Update()
         {
-            if(CanAttackPlayer()
+            if (CanSeePlayer())
             {
-                nextState = new Attack(npc, agent, anim, player);
-                state = EVENT.EXIT
-            })
-            else if (!CanseePlayer())
+                nextState = new Pursue(npc, agent, anim, player);
+                stage = EVENT.EXIT;
+            }
+            // The only place where Update can break out of itself. Set chance of breaking out at 10%.
+            else if (Random.Range(0, 100) < 10)
             {
                 nextState = new Patrol(npc, agent, anim, player);
+                stage = EVENT.EXIT; // The next time 'Process' runs, the EXIT stage will run instead, which will then return the nextState.
+            }
+        }
+
+        public override void Exit()
+        {
+            anim.ResetTrigger("isIdle"); // Makes sure that any events queued up for Idle are cleared out.
+            base.Exit();
+        }
+    }
+
+    // Constructor for Patrol state.
+    public class Patrol : State
+    {
+        int currentIndex = -1;
+        public Patrol(GameObject _npc, NavMeshAgent _agent, Animator _anim, Transform _player)
+                    : base(_npc, _agent, _anim, _player)
+        {
+            name = STATE.PATROL; // Set name of current state.
+            agent.speed = 2; // How fast your character moves ONLY if it has a path. Not used in Idle state since agent is stationary.
+            agent.isStopped = false; // Start and stop agent on current path using this bool.
+        }
+
+        public override void Enter()
+        {
+            float lastDist = Mathf.Infinity;
+            for (int i = 0; i < GameEnvironment.Singleton.Checkpoints.Count; i++)
+            {
+                GameObject thisWP = GameEnvironment.Singleton.Checkpoints[i];
+                float distance = Vector3.Distance(npc.transform.position, thisWP.transform.position);
+                if(distance < lastDist)
+                {
+                    currentIndex = i - 1;
+                    lastDist = distance;
+                }
+            }
+            anim.SetTrigger("isWalking"); // Start agent walking animation.
+            base.Enter();
+        }
+
+        public override void Update()
+        {
+            // Check if agent hasn't finished walking between waypoints.
+            if (agent.remainingDistance < 1)
+            {
+                // If agent has reached end of waypoint list, go back to the first one, otherwise move to the next one.
+                if (currentIndex >= GameEnvironment.Singleton.Checkpoints.Count - 1)
+                    currentIndex = 0;
+                else
+                    currentIndex++;
+
+                agent.SetDestination(GameEnvironment.Singleton.Checkpoints[currentIndex].transform.position); // Set agent's destination to position of next waypoint.
+            }
+
+            if (CanSeePlayer())
+            {
+                nextState = new Pursue(npc, agent, anim, player);
                 stage = EVENT.EXIT;
             }
         }
+
+        public override void Exit()
+        {
+            anim.ResetTrigger("isWalking"); // Makes sure that any events queued up for Walking are cleared out.
+            base.Exit();
+        }
     }
 
-    public override void Exit()
+    // Constructor for Pursue state.
+    public class Pursue : State
     {
-        anim.ResetTrigger("isRunning");
-        base.Exit();
-    }
-}
+        public Pursue(GameObject _npc, NavMeshAgent _agent, Animator _anim, Transform _player)
+                    : base(_npc, _agent, _anim, _player)
+        {
+            name = STATE.PURSUE;
+            agent.speed = 5;
+            agent.isStopped = false;
+        }
 
-public class Attack : State
-{
-    float rotationSpeed = 2.0f;
-    AudioSource shoot;
-    public Attack(GameObject _npc, NavMeshAgent _agent, Animator _anim, Transform _player)
-                : base(_npc, _agent, _anim, _player)
+        public override void Enter()
+        {
+            anim.SetTrigger("isRunning");
+            base.Enter();
+        }
+
+        public override void Update()
+        {
+            agent.SetDestination(player.position);
+            if (agent.hasPath)
+            {
+                if (CanAttackPlayer())
                 {
-                    name = STATE.ATTACK;
-                    shoot = _npc.GetComponent<AudioSource>();
+                    nextState = new Attack(npc, agent, anim, player);
+                    stage = EVENT.EXIT;
                 }
+                else if (!CanSeePlayer())
+                {
+                    nextState = new Patrol(npc, agent, anim, player);
+                    stage = EVENT.EXIT;
+                }
+            }
+        }
 
-    public override void Enter()
-    {
-        anim.SetTrigger("isShooting");
-        agent.isStopped = true;
-        shoot.Play();
-        base.Enter();
+        public override void Exit()
+        {
+            anim.ResetTrigger("isRunning");
+            base.Exit();
+        }
     }
 
-    public override void Update()
+    // Constructor for Attack state.
+    public class Attack : State
     {
-        base.Update();
-    }
+        float rotationSpeed = 2.0f;
+        AudioSource shoot;
 
-    public override void Exit()
-    {
-        base.Exit();
-    }
+        public Attack(GameObject _npc, NavMeshAgent _agent, Animator _anim, Transform _player)
+                    : base(_npc, _agent, _anim, _player)
+        {
+            name = STATE.ATTACK;
+            shoot = _npc.GetComponent<AudioSource>();
+        }
 
+        public override void Enter()
+        {
+            anim.SetTrigger("isShooting");
+            agent.isStopped = true;
+            shoot.Play();
+            base.Enter();
+        }
+
+        public override void Update()
+        {
+            Vector3 direction = player.position - npc.transform.position;
+            float angle = Vector3.Angle(direction, npc.transform.forward);
+            direction.y = 0;
+
+            npc.transform.rotation = Quaternion.Slerp(npc.transform.rotation,
+                                                Quaternion.LookRotation(direction),
+                                                Time.deltaTime * rotationSpeed);
+            if (!CanAttackPlayer())
+            {
+                nextState = new Idle(npc, agent, anim, player);
+                stage = EVENT.EXIT;
+            }
+        }
+
+        public override void Exit()
+        {
+            anim.ResetTrigger("isShooting");
+            shoot.Stop();
+            base.Exit();
+        }
+    }
 }
